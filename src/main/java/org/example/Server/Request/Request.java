ee72b97ea981b69b606a0e7183f5f54b6499a661
Max Khrapatiy <max.khrapatiy@gmail.com>
2023-12-05 05:48:26 +0300

    Add handlers.

--- src/main/java/org/example/Server/Request/Request.java
+++ src/main/java/org/example/Server/Request/Request.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-@ToString(includeFieldNames = true)
+@ToString()
 public class Request {
     private final static Logger myLogger = LogManager.getLogger(Request.class);
     private final static byte[] LINE_DELIMITER = new byte[]{'\r', '\n'};
